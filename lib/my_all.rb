9976c1ed6962d76446a0733c2fd0e609b1be919a
Jordan Reed <jordanreed03@gmail.com>
2020-12-02 18:37:29 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,5 +8,9 @@ def my_all?(collection)
     yield(collection[i])
     i = i + 1
   end
-  return block_return_values[0]
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
